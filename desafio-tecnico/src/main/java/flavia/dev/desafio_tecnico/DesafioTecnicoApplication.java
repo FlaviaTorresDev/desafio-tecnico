ffc68f31e6e9cb7eec9eb30c809b2c86518cbcc1
FlaviaTorresDev <flaviatorresdev@gmail.com>
2024-08-22 17:39:08 -0300

    inicializando e estruturando pacotes do projeto

---
+++ desafio-tecnico/src/main/java/flavia/dev/desafio_tecnico/DesafioTecnicoApplication.java
@@ -0,0 +1,13 @@
+package flavia.dev.desafio_tecnico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTecnicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTecnicoApplication.class, args);
+	}
+
+}
