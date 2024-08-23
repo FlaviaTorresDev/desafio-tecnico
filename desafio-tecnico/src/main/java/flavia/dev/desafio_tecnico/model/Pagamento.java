627183d52a390cef4bd7bbab65ef291663d5fb20
FlaviaTorresDev <flaviatorresdev@gmail.com>
2024-08-23 08:45:34 -0300

    adicionando a tabela de pagamento

---
+++ desafio-tecnico/src/main/java/flavia/dev/desafio_tecnico/model/Pagamento.java
@@ -0,0 +1,47 @@
+package flavia.dev.desafio_tecnico.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Pagamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int codigoDebito;
+
+    @Column(nullable = false, unique = true)
+    private String cpfCnpj;
+
+
+    private String numeroCartao;
+
+    private BigDecimal valor;
+    private boolean ativo = true;
+
+    @Enumerated(EnumType.STRING)
+    private StatusPagamento status;
+    
+    @Enumerated(EnumType.STRING)
+    private MetodoPagamento metodoPagamento;
+
+
+}
+
+public enum MetodoPagamento {
+    BOLETO, PIX, CARTAO_CREDITO, CARTAO_DEBITO
+}
+
+public enum StatusPagamento {
+    PENDENTE, PROCESSADO_SUCESSO, PROCESSADO_FALHA
+}
